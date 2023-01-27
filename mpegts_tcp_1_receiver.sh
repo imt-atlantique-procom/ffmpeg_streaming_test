753bba3da04284dc86870cb3d1e8060716c92770
Franco Liberali <franco.liberali@gmail.com>
2023-01-27 15:02:31 +0100

    reduce latency for mpegts

--- mpegts_tcp_1_receiver.sh
+++ mpegts_tcp_1_receiver.sh
@@ -1,1 +1,1 @@
-ffplay tcp://localhost:3333?listen
+ffplay -fflags nobuffer -flags low_delay -framedrop tcp://localhost:3333?listen
--- mpegts_tcp_2_sender.sh
+++ mpegts_tcp_2_sender.sh
@@ -1,1 +1,1 @@
-ffmpeg -f v4l2 -i /dev/video0 -pix_fmt yuv420p -preset ultrafast -b:v 600k -f mpegts tcp://localhost:1111
+ffmpeg -re -f v4l2 -i /dev/video0 -pix_fmt yuv420p -preset ultrafast -tune zerolatency -fflags nobuffer -b:v 600k -f mpegts tcp://localhost:1111
